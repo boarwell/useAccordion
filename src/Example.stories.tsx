7cef6c31c6e2ce64b0a7184fab34946f4105c415
boarwell <34891695+boarwell@users.noreply.github.com>
2022-03-25 01:10:18 +0900

    add Ladle
    https://www.ladle.dev/

---
+++ src/Example.stories.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import { useAccordion } from "./main.jsx";
+import "./Example.stories.css";
+
+export const Example = () => {
+  const [ref, dispatch] = useAccordion();
+  const toggle = () => {
+    dispatch({ type: "TOGGLE" });
+  };
+
+  return (
+    <section>
+      <div ref={ref} className="content">
+        <div style={{ height: "500px" }}>Content</div>
+      </div>
+      <button type="button" onClick={toggle}>
+        Toggle
+      </button>
+    </section>
+  );
+};
